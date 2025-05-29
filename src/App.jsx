6423d4dfc431eb59de56da623d7090267c64191b
00frank <00frankgarcia@gmail>
2025-05-29 06:39:14 -0300

    updating step info | onNext step receives info

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ import Home from './pages/Home'
 import Onboarding from './pages/Onboarding'
 import CollectionSettings from './pages/CollectionSettings'
 import ListCollection from './pages/ListCollection'
+import UserOptions from './pages/UserOptions'
 
 function App() {
   const [step, setStep] = useState(0)
   const [aiResponse, setAiResponse] = useState(null)
+  const [stepData, setStepData] = useState(null)
+
+  const handleStep = (newStep, data = null) => {
+    setStep(newStep)
+    if (data !== null) {
+      setStepData(data)
+    }
+  }
 
   const renderStep = () => {
     switch (step) {
       case 0:
-        return <Home onNext={() => setStep(1)} />
+        return <Home onNext={() => handleStep(1)} />
       case 1:
-        return <Onboarding onNext={() => setStep(2)} setAiResponse={setAiResponse} />
+        return <Onboarding onNext={() => handleStep(2)} setAiResponse={setAiResponse} />
       case 2:
-        return <CollectionSettings onNext={() => setStep(3)} aiResponse={aiResponse} />
+        return <CollectionSettings onNext={() => handleStep(3)} aiResponse={aiResponse} />
       case 3:
-        return <ListCollection onNext={() => setStep(0)} aiResponse={aiResponse} />
+        return <ListCollection onNext={(selectedItems) => handleStep(4, selectedItems)} aiResponse={aiResponse} />
+      case 4:
+        return <UserOptions onNext={() => handleStep(5)} selectedItems={stepData} />
       default:
         return <Home />
     }
@@ -29,7 +40,7 @@ function App() {
       <PhoneFrame>
         {renderStep()}
       </PhoneFrame>
-      <p className='absolute bottom-2 right-2/3 w-16 h-16 flex items-center justify-center text-center bg-white rounded-full border-4 text-2xl font-bold'>{step}</p>
+      <p className='absolute top-2 left-2 w-16 h-16 flex items-center justify-center text-center bg-white rounded-full border-4 text-2xl font-bold'>{step}</p>
     </>
   )
 }
